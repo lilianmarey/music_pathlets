3236b9b0aa0360dd0701ec73aa60d96637c689b4
Lilian Marey <68290011+lilianmarey@users.noreply.github.com>
2025-05-05 11:48:20 +0200

    Update build_events_candidates.py

--- processing/build_events_candidates.py
+++ processing/build_events_candidates.py
@@ -9,36 +9,36 @@ from globals import *
 
 ########################################################################
 
-X = import_pickle(f"{XXXX_processed_path}X.pkl")
-Y = import_pickle(f"{XXXX_processed_path}Y.pkl")
-all_users = import_pickle(f"{XXXX_processed_path}all_users.pkl")
-all_tags = import_pickle(f"{XXXX_processed_path}all_tags.pkl")
+X = import_pickle(f"{DEEZER_processed_path}X.pkl")
+Y = import_pickle(f"{DEEZER_processed_path}Y.pkl")
+all_users = import_pickle(f"{DEEZER_processed_path}all_users.pkl")
+all_tags = import_pickle(f"{DEEZER_processed_path}all_tags.pkl")
 
 ########################################################################
 
 train_appearance_candidates, train_disappearance_candidates = compute_candidates(
-    X, Y, all_users, all_tags, shift=1, K=XXXX_K
+    X, Y, all_users, all_tags, shift=1, K=DEEZER_K
 )
 test_appearance_candidates, test_disappearance_candidates = compute_candidates(
-    X, Y, all_users, all_tags, shift=0, K=XXXX_K
+    X, Y, all_users, all_tags, shift=0, K=DEEZER_K
 )
 
 ########################################################################
 
 save_pickle(
-    f"{XXXX_processed_path}appearance_candidates_train.pkl",
+    f"{DEEZER_processed_path}appearance_candidates_train.pkl",
     train_appearance_candidates,
 )
 save_pickle(
-    f"{XXXX_processed_path}disappearance_candidates_train.pkl",
+    f"{DEEZER_processed_path}disappearance_candidates_train.pkl",
     train_disappearance_candidates,
 )
 
 save_pickle(
-    f"{XXXX_processed_path}appearance_candidates_test.pkl",
+    f"{DEEZER_processed_path}appearance_candidates_test.pkl",
     test_appearance_candidates,
 )
 save_pickle(
-    f"{XXXX_processed_path}disappearance_candidates_test.pkl",
+    f"{DEEZER_processed_path}disappearance_candidates_test.pkl",
     test_disappearance_candidates,
 )
