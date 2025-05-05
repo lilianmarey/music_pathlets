0269be3327e5edbe7bea74708e35fbf074e35d65
Lilian Marey <68290011+lilianmarey@users.noreply.github.com>
2025-05-05 11:50:03 +0200

    Update compute_pathlet_embedding.py

--- modeling/compute_pathlet_embedding.py
+++ modeling/compute_pathlet_embedding.py
@@ -9,21 +9,21 @@ from globals import *
 
 ####################################################################################
 
-appearance_D = import_pickle(f"{XXXX_processed_path}D_appearance.pkl")
-disappearance_D = import_pickle(f"{XXXX_processed_path}D_disappearance.pkl")
+appearance_D = import_pickle(f"{DEEZER_processed_path}D_appearance.pkl")
+disappearance_D = import_pickle(f"{DEEZER_processed_path}D_disappearance.pkl")
 
-users_tag_ranking = import_pickle(f"{XXXX_processed_path}users_tag_ranking.pkl")
+users_tag_ranking = import_pickle(f"{DEEZER_processed_path}users_tag_ranking.pkl")
 train_appearance_trajectories = import_pickle(
-    f"{XXXX_processed_path}appearance_trajectories_train.pkl"
+    f"{DEEZER_processed_path}appearance_trajectories_train.pkl"
 )
 train_disappearance_trajectories = import_pickle(
-    f"{XXXX_processed_path}disappearance_trajectories_train.pkl"
+    f"{DEEZER_processed_path}disappearance_trajectories_train.pkl"
 )
 test_appearance_trajectories = import_pickle(
-    f"{XXXX_processed_path}appearance_trajectories_test.pkl"
+    f"{DEEZER_processed_path}appearance_trajectories_test.pkl"
 )
 test_disappearance_trajectories = import_pickle(
-    f"{XXXX_processed_path}disappearance_trajectories_test.pkl"
+    f"{DEEZER_processed_path}disappearance_trajectories_test.pkl"
 )
 
 ####################################################################################
@@ -32,23 +32,23 @@ compute_embeddings(
     appearance_D,
     users_tag_ranking,
     train_appearance_trajectories,
-    f"{XXXX_processed_path}appearance_embeddings_train.csv",
+    f"{DEEZER_processed_path}appearance_embeddings_train.csv",
 )
 compute_embeddings(
     disappearance_D,
     users_tag_ranking,
     train_disappearance_trajectories,
-    f"{XXXX_processed_path}disappearance_embeddings_train.csv",
+    f"{DEEZER_processed_path}disappearance_embeddings_train.csv",
 )
 compute_embeddings(
     appearance_D,
     users_tag_ranking,
     test_appearance_trajectories,
-    f"{XXXX_processed_path}appearance_embeddings_test.csv",
+    f"{DEEZER_processed_path}appearance_embeddings_test.csv",
 )
 compute_embeddings(
     disappearance_D,
     users_tag_ranking,
     test_disappearance_trajectories,
-    f"{XXXX_processed_path}disappearance_embeddings_test.csv",
+    f"{DEEZER_processed_path}disappearance_embeddings_test.csv",
 )
