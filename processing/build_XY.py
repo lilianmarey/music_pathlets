f41259183de1757c9a4593eac544398529b9eda0
Lilian Marey <68290011+lilianmarey@users.noreply.github.com>
2025-05-05 11:47:40 +0200

    Update build_XY.py

--- processing/build_XY.py
+++ processing/build_XY.py
@@ -18,11 +18,11 @@ tqdm.pandas()
 ########################################################################
 
 # Import raw data
-df = pd.read_csv("data/XXXX/histories.csv")
+df = pd.read_csv("data/DEEZER/histories.csv")
 
 # Compute time periods
 df["time_period"] = df["ts"].progress_apply(
-    lambda ts: timestamp_to_time_period(ts, XXXX_time_periods)
+    lambda ts: timestamp_to_time_period(ts, DEEZER_time_periods)
 )
 
 # Define user sets
@@ -31,7 +31,7 @@ all_tags = df.tag.unique().tolist()
 
 # Build X
 X, X_counts, valid_users = compute_allocations(
-    df, all_users, all_tags, XXXX_time_periods, XXXX_stream_threshold
+    df, all_users, all_tags, DEEZER_time_periods, DEEZER_stream_threshold
 )
 print(f"Number of valid users : {len(valid_users)}")
 
@@ -40,16 +40,16 @@ Y = dict()
 
 for user_id in valid_users:
     for tag in all_tags:
-        Y[(user_id, tag)] = X[(user_id, XXXX_K, tag)]
-        del X[(user_id, XXXX_K, tag)]
+        Y[(user_id, tag)] = X[(user_id, DEEZER_K, tag)]
+        del X[(user_id, DEEZER_K, tag)]
 
 # Clean df
 df = delete_non_listen_genres(df, X)
 
 # Save data
-df.to_csv(f"{XXXX_processed_path}cut_histories.pkl", index=False)
-save_pickle(f"{XXXX_processed_path}all_users.pkl", valid_users)
-save_pickle(f"{XXXX_processed_path}all_tags.pkl", all_tags)
-save_pickle(f"{XXXX_processed_path}X.pkl", X)
-save_pickle(f"{XXXX_processed_path}X_counts.pkl", X_counts)
-save_pickle(f"{XXXX_processed_path}Y.pkl", Y)
+df.to_csv(f"{DEEZER_processed_path}cut_histories.pkl", index=False)
+save_pickle(f"{DEEZER_processed_path}all_users.pkl", valid_users)
+save_pickle(f"{DEEZER_processed_path}all_tags.pkl", all_tags)
+save_pickle(f"{DEEZER_processed_path}X.pkl", X)
+save_pickle(f"{DEEZER_processed_path}X_counts.pkl", X_counts)
+save_pickle(f"{DEEZER_processed_path}Y.pkl", Y)
