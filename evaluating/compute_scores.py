5af652d1f9d0bba8592b07b48b35e7d9fd440d27
Lilian Marey <68290011+lilianmarey@users.noreply.github.com>
2025-05-05 11:51:07 +0200

    Update compute_scores.py

--- evaluating/compute_scores.py
+++ evaluating/compute_scores.py
@@ -15,39 +15,39 @@ from globals import *
 
 ########################################################################
 
-all_users = import_pickle(f"{XXXX_processed_path}all_users.pkl")
-all_tags = import_pickle(f"{XXXX_processed_path}all_tags.pkl")
+all_users = import_pickle(f"{DEEZER_processed_path}all_users.pkl")
+all_tags = import_pickle(f"{DEEZER_processed_path}all_tags.pkl")
 
-X = import_pickle(f"{XXXX_processed_path}X.pkl")
-Y = import_pickle(f"{XXXX_processed_path}Y.pkl")
-X_counts = import_pickle(f"{XXXX_processed_path}X_counts.pkl")
+X = import_pickle(f"{DEEZER_processed_path}X.pkl")
+Y = import_pickle(f"{DEEZER_processed_path}Y.pkl")
+X_counts = import_pickle(f"{DEEZER_processed_path}X_counts.pkl")
 
-D_appearance = import_pickle(f"{XXXX_processed_path}D_appearance.pkl")
-D_disappearance = import_pickle(f"{XXXX_processed_path}D_disappearance.pkl")
+D_appearance = import_pickle(f"{DEEZER_processed_path}D_appearance.pkl")
+D_disappearance = import_pickle(f"{DEEZER_processed_path}D_disappearance.pkl")
 appearance_candidates_dict_train = import_pickle(
-    f"{XXXX_processed_path}appearance_candidates_train.pkl"
+    f"{DEEZER_processed_path}appearance_candidates_train.pkl"
 )
 appearance_candidates_dict_test = import_pickle(
-    f"{XXXX_processed_path}appearance_candidates_test.pkl"
+    f"{DEEZER_processed_path}appearance_candidates_test.pkl"
 )
 disappearance_candidates_dict_train = import_pickle(
-    f"{XXXX_processed_path}disappearance_candidates_train.pkl"
+    f"{DEEZER_processed_path}disappearance_candidates_train.pkl"
 )
 disappearance_candidates_dict_test = import_pickle(
-    f"{XXXX_processed_path}disappearance_candidates_test.pkl"
+    f"{DEEZER_processed_path}disappearance_candidates_test.pkl"
 )
 
 df_appearance_embeddings_train = pd.read_csv(
-    f"{XXXX_processed_path}appearance_embeddings_train.csv", index_col=0
+    f"{DEEZER_processed_path}appearance_embeddings_train.csv", index_col=0
 )
 df_appearance_embeddings_test = pd.read_csv(
-    f"{XXXX_processed_path}appearance_embeddings_test.csv", index_col=0
+    f"{DEEZER_processed_path}appearance_embeddings_test.csv", index_col=0
 )
 df_disappearance_embeddings_train = pd.read_csv(
-    f"{XXXX_processed_path}disappearance_embeddings_train.csv", index_col=0
+    f"{DEEZER_processed_path}disappearance_embeddings_train.csv", index_col=0
 )
 df_disappearance_embeddings_test = pd.read_csv(
-    f"{XXXX_processed_path}disappearance_embeddings_test.csv", index_col=0
+    f"{DEEZER_processed_path}disappearance_embeddings_test.csv", index_col=0
 )
 
 
@@ -57,12 +57,12 @@ df_appearance_embeddings_train.columns = D_disappearance
 df_disappearance_embeddings_train.columns = D_appearance
 
 Y_train = np.array(
-    [[X[(user_id, XXXX_K - 1, tag)] for tag in all_tags] for user_id in all_users]
+    [[X[(user_id, DEEZER_K - 1, tag)] for tag in all_tags] for user_id in all_users]
 )
 Y_test = np.array([[Y[(user_id, tag)] for tag in all_tags] for user_id in all_users])
 
 X_prev = np.array(
-    [[X[(user_id, XXXX_K - 2, tag)] for tag in all_tags] for user_id in all_users]
+    [[X[(user_id, DEEZER_K - 2, tag)] for tag in all_tags] for user_id in all_users]
 )
 
 ########################################################################
@@ -71,20 +71,20 @@ baseline = Popularity(all_tags)
 baseline.fit(X)
 Y_pred = baseline.predict(all_users)
 make_report(Y_pred, Y_test, Y_train).to_csv(
-    f"{XXXX_results_path}Popularity.csv", index=False
+    f"{DEEZER_results_path}Popularity.csv", index=False
 )
 
-baseline = NMFBaseline(all_tags, X_counts, XXXX_K)
+baseline = NMFBaseline(all_tags, X_counts, DEEZER_K)
 Y_pred = baseline.predict(all_users)
 make_report(Y_pred, Y_test, Y_train).to_csv(
-    f"{XXXX_results_path}NMFBaseline.csv", index=False
+    f"{DEEZER_results_path}NMFBaseline.csv", index=False
 )
 
 baseline = Previous(all_tags)
 baseline.fit(X)
 Y_pred = baseline.predict(all_users)
 make_report(Y_pred, Y_test, X_prev).to_csv(
-    f"{XXXX_results_path}Previous.csv", index=False
+    f"{DEEZER_results_path}Previous.csv", index=False
 )
 
 appearance_pred_dict = build_prediction_for_plug(
@@ -102,5 +102,5 @@ baseline = PreviousPlug(all_tags, appearance_pred_dict, disappearance_pred_dict)
 baseline.fit(X)
 Y_pred = baseline.predict(all_users)
 make_report(Y_pred, Y_test, Y_train).to_csv(
-    f"{XXXX_results_path}PreviousPlug.csv", index=False
+    f"{DEEZER_results_path}PreviousPlug.csv", index=False
 )
